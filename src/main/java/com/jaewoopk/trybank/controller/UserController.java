e56d88e7ac878f0e0fb6a4cd4abd92ef424e5c44
jaewoopk <jaewoopk000@naver.com>
2023-10-05 18:11:51 +0900

    Day 4 Seperate packages and implement some User works

---
+++ src/main/java/com/jaewoopk/trybank/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.jaewoopk.trybank.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @RequestMapping("/signup")
+    public String create() {
+        return "user/signup";
+    }
+
+    @RequestMapping("/idcheck")
+
+
+}
---
+++ src/main/java/com/jaewoopk/trybank/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.jaewoopk.trybank.dao;
+
+public class UserDao {
+
+}

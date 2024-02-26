077fb2d1040947f11aaa7084a68a5f95b91464d7
jaewoopk <jaewoopk000@naver.com>
2024-02-26 16:35:08 +0900

    Day 15 Revise UnitTest with beans, lifecycle

---
+++ src/test/java/java/hello/core/CoreApplicationTests.java
@@ -0,0 +1,10 @@
+package java.hello.core;
+
+//@SpringBootTest
+//class CoreApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
---
+++ src/test/java/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,10 @@
+package java.hello.core.scan;
+
+public class AutoAppConfigTest {
+//    @Test
+//    void basicScan() {
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+//        MemberService memberService = ac.getBean(MemberService.class);
+//        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+//    }
+}
